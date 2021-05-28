a3fe9c4d5e9b85dbd00b780babeb99d86a5d0919
AtroX <46425840+DavidWild02@users.noreply.github.com>
2021-05-28 10:32:01 +0200

    Copy paste from school pc

--- Hnefatafl/Hnefatafl/Program.cs
+++ Hnefatafl/Hnefatafl/Program.cs
@@ -1,12 +1,6 @@
-﻿/*
- * Created by SharpDevelop.
- * User: WiDa0502
- * Date: 23.04.2021
- * Time: 09:18
- * 
- * To change this template use Tools | Options | Coding | Edit Standard Headers.
- */
-using System;
+﻿using System;
+using System.Collections;
+using System.Linq;
 
 namespace Hnefatafl
 {
@@ -20,13 +14,47 @@ namespace Hnefatafl
 		}
 		
 		public static Move parseMove(string s) {
-			
+			string[] positions = s.ToUpper().Trim().Split(new string[] {"=>"}, 2, StringSplitOptions.RemoveEmptyEntries);
+			Move move;
+			if (Enumerable.Range('A', 'K').Contains(positions[0][0])) {
+				move.y = (int)positions[0][0] - 65;
+			} else {
+				throw Exception("y position not in range of A-K. y must be before x");
+			}
+			if (Enumerable.Range('A', 'K').Contains(positions[1][0])) {
+				move.desty = (int)positions[1][0] - 65;
+			} else {
+				throw Exception("desty position not in range of A-K. desty must be before destx");
+			}
+			try {
+				positions[0].Remove(0);
+				positions[1].Remove(0);
+				move.x = Convert.ToInt32(positions[0]);
+				move.destx = Convert.ToInt32(positions[1]);
+				return move;
+			}
+			catch (Exception e) {
+				Console.WriteLine(e.Message);
+				throw Exception("Was not able to Convert the x and destx positions");
+			}
 		}
 		
 		public static void Main(string[] args)
 		{
 			Game game = new Game();
 			game.print();
+			Console.Write("Enter the moves (\n\n\nFormat COL ROW => COL ROW dest)\n");
+			
+			int origRow;
+			int origCol;
+			while (true) {
+				origRow = Console.CursorTop;
+				origCol = Console.CursorLeft;
+				Console.SetCursorPosition(0, 0);
+				game.print();
+				Console.SetCursorPosition(origCol, origRow);
+				
+			}
 			
 			
 			
